3715ab21d70b73a46a5e8110be7cb4c8f26c4ec1
Eric Crescioni <ecam900@gmail.com>
2020-07-31 17:24:34 -0400

    Created input and fetch results from API.

--- src/components/MainPageLayout.js
+++ src/components/MainPageLayout.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Navs from './Navs';
+import Title from './Title';
 
 const MainPageLayout = ({ children }) => {
   return (
     <div>
       <Navs />
+      <Title
+        title='Box Office'
+        subtitle='Are you looking for a Movie or an Actor?'
+      />
       {children}
     </div>
   );
