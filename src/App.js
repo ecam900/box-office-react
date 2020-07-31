dc86dfa4aedd20a9691a99ecbdabb576de0b3cd6
Eric Crescioni <ecam900@gmail.com>
2020-07-31 17:10:27 -0400

    Add MainPageComponent to split layouts.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Navs from './components/Navs';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 
 function App() {
   return (
-    <div>
-      <Navs />
-      <Switch>
-        <Route path='/' exact>
-          <Home />
-        </Route>
-        <Route path='/starred' exact>
-          <Starred />
-        </Route>
+    <Switch>
+      <Route path='/' exact>
+        <Home />
+      </Route>
+      <Route path='/starred' exact>
+        <Starred />
+      </Route>
 
-        <Route>This is 404 page</Route>
-      </Switch>
-    </div>
+      <Route>This is 404 page</Route>
+    </Switch>
   );
 }
 
