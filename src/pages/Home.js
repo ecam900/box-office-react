57a54d2feda9bbd58bb4392237825b3029aad2b3
Eric Crescioni <ecam900@gmail.com>
2020-07-31 17:35:57 -0400

    Display show result. Small fetch refactor.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,43 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
 
   const onInputChange = (e) => {
     setInput(e.target.value);
   };
 
   const onSearch = () => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then((res) => res.json())
-      .then((result) => {
-        console.log(result);
-      });
+    apiGet(`/search/shows?q=${input}`).then((result) => {
+      setResults(result);
+    });
   };
 
   const onKeyDown = (e) => {
     if (e.keyCode === 13) {
       onSearch();
     }
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results! Whaaaaaaaat!?</div>;
+    }
+
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map((item) => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+
+    return null;
   };
 
   return (
@@ -33,6 +51,7 @@ const Home = () => {
       <button type='button' onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
